b328b18039a1d9b7e9e8ae83af96b62f9ac9901f
Gustavo Paez <paez74@live.com.mx>
2020-11-24 19:01:53 -0600

    added basic socket template on server/client

--- DNAComparator-Server/main.c
+++ DNAComparator-Server/main.c
@@ -1,5 +1,9 @@
+#include<io.h>
 #include <stdio.h>
 #include <omp.h>
+#include <winsock2.h> // en linux es sys-sock.h
+
+#pragma comment(lib,"ws2_32.lib")
 
 // funcion para leer archivo y partir las lineas
 
@@ -18,7 +22,53 @@ X secuencias mapeadas
 X secuencias no mapeadas
 */
 //funcion main
-int main() {
+int main(int argc, char *argv[]) 
+{
+    WSADATA wsa;
+    SOCKET s, new_socket;
+    struct sockaddr_in server, client;
+    int c;
+    char *message;
+
+    if(WSAStartup(MAKEWORD(2,2),&wsa != 0))
+    {
+        printf("Failed");
+        return 1;
+    }
+
+    if((s = socket(AF_INET,SOCK_STREAM,0)) == INVALID_SOCKET)
+    {
+        printf("Error creatign socket");
+    }
+
+    printf("Socket created");
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(6666); // numero de puerto
+
+
+    if (bind (s, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
+    {
+        printf("bind error");
+    }
+
+    listern(s,3);
+
+    c = sizeof(struct sockaddr_in);
+    while((new_socket = accept(s, (struct sockaddr *)&client, &c)) != INVALID_SOCKET)
+    {
+        puts("Connection Accepted");
+        message = "Hello socket\n";
+        send(new_socket, message, strlen(message), 0);
+    }
+
+
+ if(new_socket == INVALID_SOCKET)
+ {
+     printf("Trono el socket");
+ }
+
 
     #pragma omp parallel
     {
